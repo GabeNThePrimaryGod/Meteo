492fbb77ed28dedea45574cefafa17793685f39a
Kennedy <bryan.binet@live.fr>
2021-01-24 09:05:10 +0100

    Intégration map Index.html

--- assets/js/map-bryan.js
+++ assets/js/map-bryan.js
@@ -7,8 +7,8 @@
     var path = d3.geoPath().projection(projection);
 
   // load data  
-var worldmap = d3.json("assets/data/countries.geojson");
-var cities = d3.csv("assets/data/cities.csv");
+var worldmap = d3.json("/Meteo/assets/data/countries.geojson");
+var cities = d3.csv("/Meteo/assets/data/cities.csv");
 
 Promise.all([worldmap, cities]).then(function(values){    
  // draw map
@@ -40,48 +40,5 @@ Promise.all([worldmap, cities]).then(function(values){
         .attr("class","labels");
 
 	});
-}
-
-function createDotMap(lat,long,sca,cont,w,h){
-  var svg = d3.select("div#"+cont).append("svg").attr("preserveAspectRatio", "xMinYMin meet").style("background-color","#333333")
-  .attr("viewBox", "0 0 " + w + " " + h).style("display","flex")
-  .classed("svg-content", true);
-  var projection = d3.geoMercator().translate([w/2, h/2]).scale(sca).center([lat,long]);
-  var path = d3.geoPath().projection(projection);
-
-// load data  
-var worldmap = d3.json("assets/data/countries.geojson");
-var cities = d3.csv("assets/data/cities.csv");
-
-Promise.all([worldmap, cities]).then((values) => {
-// draw map
-  svg.selectAll("path")
-      .data(values[0].features)
-      .enter()
-      .append("path")
-      .attr("class","continent")
-      .attr("d", path),
-// draw points
-  svg.selectAll("circle")
-      .data(values[1])
-      .enter()
-      .append("circle")
-      .attr("class","circles")
-      .attr("cx", (d) => projection([d.Longitude, d.Lattitude])[0])
-      .attr("cy", (d) => projection([d.Longitude, d.Lattitude])[1])
-      .attr("r", "1px"),
-// add labels
-  svg.selectAll("text")
-      .data(values[1])
-      .enter()
-      .append("text")
-      .text(function(d) {
-                  return d.City;
-             })
-      .attr("x", (d) => projection([d.Longitude, d.Lattitude])[0] + 5)
-      .attr("y", (d) => projection([d.Longitude, d.Lattitude])[1] + 15)
-      .attr("class","labels");
-
-});
 }
 
